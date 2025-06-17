3bac31d85e4cce28c44e87e7c3c8020a926ba91f
RayyesAbed <abedray1567@gmail.com>
2025-06-17 17:03:06 +0200

    Created my navigation items array constant to made my code in the Navbar cleaner

---
+++ src/constants/navigationItems.jsx
@@ -0,0 +1,8 @@
+const navigationItems = [
+  { path: "/", name: "Welcome" },
+  { path: "/projects", name: "Projects" },
+  { path: "/about", name: "About" },
+  { path: "/contact", name: "Contact" },
+];
+
+export default navigationItems;
