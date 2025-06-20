bb29756b7162c6f3109305926a70ac694998864d
RayyesAbed <abedray1567@gmail.com>
2025-06-20 11:34:43 +0200

    Created a projects array to be used when mapping its elements in Projects Component for cleaner code

---
+++ src/constants/projects.jsx
@@ -0,0 +1,13 @@
+import PlanItImage from "/PlanItImage.png";
+
+const projects = [
+  {
+    img: PlanItImage,
+    imgAlt: "PlanIt Image",
+    developmentStatus: "Under Development",
+    projectHeader: "PlanIt - My Habit Journey Planner",
+    link: "#", // Will be changed later
+  },
+];
+
+export default projects;
