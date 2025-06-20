7d97f3e9aa0c233c09fb47da30a82da2732569c2
RayyesAbed <abedray1567@gmail.com>
2025-06-20 14:40:53 +0200

    Added the project link and improved the naming of the project image interface for better separation

--- src/constants/projects.jsx
+++ src/constants/projects.jsx
@@ -1,12 +1,12 @@
-import PlanItImage from "/PlanItImage.png";
+import PlanItImageInterface from "/PlanItImageInterface.png";
 
 const projects = [
   {
-    img: PlanItImage,
+    img: PlanItImageInterface,
     imgAlt: "PlanIt Image",
     developmentStatus: "Under Development",
     projectHeader: "PlanIt - My Habit Journey Planner",
-    link: "#", // Will be changed later
+    link: "planit",
   },
 ];
 
