1a5f6967d7b4e0f5d6335687734778c18f67d10e
RayyesAbed <abedray1567@gmail.com>
2025-06-22 14:07:52 +0200

    Created the About component, imported its CSS file, animated the section when viewed, and added the following:
    - An image of me
    - A brief summary about who I am
    - A later to be added Resume link

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,45 @@
+import "./About.css";
+import AboutImage from "/About.jpg";
+import { motion } from "framer-motion";
+
+const About = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+      id="about"
+    >
+      <img src={AboutImage} alt="My About image" />
+      <h1>My Life</h1>
+      <p>
+        Since my childhood, I have always seen computers as a treasure. When I
+        was 15, I learned HTML for the first time and the experience of writing
+        my first website and the browser rendering it, was mind-blowing, a true
+        game changer. it felt like I had discovered a hidden superpower inside
+        my laptop.
+      </p>
+      <p>
+        As I entered the university, I chose Computer Engineering as my major
+        and began learning about core programming concepts like data structures,
+        algorithms, and more. Those foundational courses had a huge impact on me
+        and inspired my decision to transition fully into Computer Science.
+      </p>
+      <p>
+        Afterwards I took a course in frontend development with React and not
+        long after, I found an internship where I was introduced to both
+        frontend and backend development. This is where I found my passion, and
+        this is where I remembered my young self but this time, I found my path.
+        Thanks to this experience, I decided to pursue a career a Full Stack Web
+        Developer.
+      </p>
+      <p>
+        You can take a look at my resume here: <a href="#">Resume &#8599;</a>{" "}
+        {/* Resume link to be added later */}
+      </p>
+    </motion.div>
+  );
+};
+
+export default About;
