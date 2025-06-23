d02f6131c97f1e50cf57b3e6c2b00674e91ba39e
RayyesAbed <abedray1567@gmail.com>
2025-06-23 06:29:00 +0200

    Created a social media links array, and used it in Contact component for cleaner code

---
+++ src/constants/socialMediaLinks.jsx
@@ -0,0 +1,19 @@
+import GitHubIcon from "/github-mark.png";
+import LinkedInLogo from "/LI-Logo.png";
+
+const socialMediaLinks = [
+  {
+    icon: GitHubIcon,
+    alt: "GitHub Icon",
+    className: "gitHub-icon",
+    href: "https://github.com/RayyesAbed",
+  },
+  {
+    icon: LinkedInLogo,
+    alt: "LinkedIn Icon",
+    className: "linkedIn-icon",
+    href: "https://de.linkedin.com/in/abdallah-alrayyes-22492818a",
+  },
+];
+
+export default socialMediaLinks;
