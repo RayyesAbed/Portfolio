d198c43b35af015ff6688aaac9ee6395943d0cd9
RayyesAbed <abedray1567@gmail.com>
2025-06-24 14:34:58 +0200

    Deleted old useStates variables and used instead useActionState hook offered in React 19 to handle form submission status, initialized it with an object, added name attributes, as well as showed a dynamic response

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,57 +1,19 @@
+"use client";
 import "./Contact.css";
 import socialMediaLinks from "../../constants/socialMediaLinks";
 import CircularProgress from "@mui/material/CircularProgress";
 import ContactImage from "/Contact.jpg";
-import emailjs from "@emailjs/browser";
-import { useState } from "react";
+import { useActionState } from "react";
+import sendEmail from "../../action/sendEmail";
 import { motion } from "framer-motion";
 
 const Contact = () => {
   document.title = "ABED | Contact";
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [isPending, setIsPending] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [isSuccessful, setIsSuccessful] = useState(false);
-
-  const handleChangeName = (event) => setName(event.target.value);
-  const handleChangeEmail = (event) => setEmail(event.target.value);
-  const handleChangeMessage = (event) => setMessage(event.target.value);
-
-  const handleSendEmail = async (event) => {
-    event.preventDefault();
-
-    setIsPending(true);
-
-    const templateParams = {
-      from_name: name,
-      reply_to: email,
-      message: message,
-    };
-
-    emailjs
-      .send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        templateParams,
-        { publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY }
-      )
-      .then(() => {
-        setIsPending(false);
-        setIsSuccessful(true);
-        setIsError(false);
-        setName("");
-        setEmail("");
-        setMessage("");
-      })
-      .catch((error) => {
-        setIsPending(false);
-        setIsError(true);
-        console.error("Error details:", error);
-      });
-  };
+  const [state, formAction, isPending] = useActionState(sendEmail, {
+    success: null,
+    message: "",
+  });
 
   return (
     <motion.div
@@ -62,36 +24,21 @@ const Contact = () => {
       id="contact"
     >
       <section>
-        <form onSubmit={handleSendEmail} id="contact-form">
+        <form action={formAction} id="contact-form">
           <h1>Get in touch</h1>
-          <input
-            type="text"
-            placeholder="Your Name"
-            value={name}
-            onChange={handleChangeName}
-            required
-          />
-          <input
-            type="email"
-            placeholder="Your Email"
-            value={email}
-            onChange={handleChangeEmail}
-            required
-          />
-          <textarea
-            placeholder="Your Message"
-            value={message}
-            onChange={handleChangeMessage}
-            required
-          />
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea placeholder="Your Message" name="message" required />
           <button disabled={isPending}>
             {isPending ? <CircularProgress color="gray" size={30} /> : "Submit"}
           </button>
-          {isSuccessful && !isPending && (
-            <p id="successful-email-text">Email sent successfully!</p>
-          )}
-          {isError && !isPending && (
-            <p id="failure-email-text">Error while sending email</p>
+          {state.message && (
+            <p
+              id="email-text"
+              className={`${state.success ? "success" : "failure"}`}
+            >
+              {state.message}
+            </p>
           )}
         </form>
       </section>
