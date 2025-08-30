3a691b641bfa7a137c5e1e01f8748bfdb2d38074
RayyesAbed <abedray1567@gmail.com>
2025-08-30 13:34:33 +0200

    Outsourced Images with Skeleton for better reusability

---
+++ src/components/loading/ImageWithSkeleton.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const ImageWithSkeleton = ({
+  height,
+  width,
+  isSkeletonCircle,
+  imgSrc,
+  imgAlt,
+}) => {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <>
+      {!loaded && (
+        <Skeleton
+          height={height}
+          width={width}
+          circle={isSkeletonCircle}
+          baseColor="#666"
+          highlightColor="#888"
+        />
+      )}
+      <img
+        src={imgSrc}
+        alt={imgAlt}
+        onLoad={() => setLoaded(true)}
+        style={loaded ? { display: "block" } : { display: "none" }}
+      />
+    </>
+  );
+};
+
+export default ImageWithSkeleton;
