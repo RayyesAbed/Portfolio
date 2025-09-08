e256057c64721edc3e3360589cb0058341fcf220
RayyesAbed <abedray1567@gmail.com>
2025-09-08 12:41:48 +0200

    Created a static project assets and stored the main image link and the link

---
+++ src/constants/projectAssets.jsx
@@ -0,0 +1,10 @@
+import PlanItImageInterface from "/PlanItImageInterface.jpg";
+
+const projectAssets = {
+  planit: {
+    mainImage: PlanItImageInterface,
+    link: "planit",
+  },
+};
+
+export default projectAssets;
