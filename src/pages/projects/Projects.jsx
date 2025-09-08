360c492e92bb5aaf6933d7a20ea2c7c7a475098a
RayyesAbed <abedray1567@gmail.com>
2025-09-08 12:41:59 +0200

    Used translated projects and got the project static assets and passed them to ProjectInterface

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,10 +1,15 @@
 import "./Projects.css";
 import ProjectInterface from "../../components/projectInterface/ProjectInterface";
-import projects from "../../constants/projects";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
+import projectAssets from "../../constants/projectAssets";
 
 const Projects = () => {
-  document.title = "ABED | Projects";
+  const { t } = useTranslation();
+
+  document.title = t("projects.title");
+
+  const projects = t("projects.list", { returnObjects: true });
 
   return (
     <motion.div
@@ -14,16 +19,19 @@ const Projects = () => {
       transition={{ duration: 0.3 }}
       id="projects"
     >
-      {projects.map((project) => (
-        <ProjectInterface
-          key={project.projectHeader}
-          img={project.img}
-          imgAlt={project.imgAlt}
-          developmentStatus={project.developmentStatus}
-          projectHeader={project.projectHeader}
-          link={project.link}
-        />
-      ))}
+      {projects.map((project) => {
+        const staticAssets = projectAssets[project.projectName];
+        return (
+          <ProjectInterface
+            key={project.projectHeader}
+            img={staticAssets.mainImage}
+            imgAlt={project.imgAlt}
+            developmentStatus={project.developmentStatus}
+            projectHeader={project.projectHeader}
+            link={staticAssets.link}
+          />
+        );
+      })}
     </motion.div>
   );
 };
