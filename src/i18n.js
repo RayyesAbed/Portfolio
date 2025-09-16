d35709d2ea8ea4da8e0f7060d640725f6267f50b
RayyesAbed <abedray1567@gmail.com>
2025-09-16 18:12:00 +0200

    Imported and used the detect computer language function in lng attribute

--- src/i18n.js
+++ src/i18n.js
@@ -2,13 +2,14 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next"; // React's bridge to connect i18n with React
 import en from "./locales/en/translation.json";
 import de from "./locales/de/translation.json";
+import detectComputerLanguage from "./utils/detectComputerLanguage";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     de: { translation: de },
   },
-  lng: "en", // the default language
+  lng: detectComputerLanguage(), // use the computer's language
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
